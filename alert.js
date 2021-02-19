bcb6d08f711313699300a218482ff9c6ffbf9687
Dmitrijs1710 <79305071+Dmitrijs1710@users.noreply.github.com>
2021-02-19 11:43:34 +0200

    Made some changes and added a file

---
+++ alert.js
@@ -0,0 +1,13 @@
+'use strict';
+let message, name = 'John',
+    age = 25;
+message = 'hello';
+message = name;
+prompt("write a numer",
+    let MyBirthDate);
+alert(MyBirthDate);
+if (confirm('do you know answer?')) {
+    alert(MyBirthDate - 4);
+} else {
+    alert("got to hell");
+}
